76e86710dea37be1f66caa1d6124077b7bdaebbe
poltuiu@14d9d42f-b097-442a-9da6-5b64999cc983 <poltuiu@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-07-03 01:07:20 +0000

    Widget master class

---
+++ bb5/visu2d/Widget.cc
@@ -0,0 +1,218 @@
+/*
+ ** TowBowlTactics, an adaptation of the tabletop game Blood Bowl.
+ ** 
+ ** Copyright (C) 2006 The TBT Team.
+ ** 
+ ** This program is free software; you can redistribute it and/or
+ ** modify it under the terms of the GNU General Public License
+ ** as published by the Free Software Foundation; either version 2
+ ** of the License, or (at your option) any later version.
+ ** 
+ ** The complete GNU General Public Licence Notice can be found as the
+ ** `NOTICE' file in the root directory.
+ ** 
+ ** The TBT Team consists of people listed in the `AUTHORS' file.
+ */
+
+ #include "Widget.hh"
+
+ Focus Widget::globfocus;
+
+ Widget::Widget(SDL_Surface* screen, Widget* father)
+ {
+   this->screen = screen;
+   this->x = 0;  this->y = 0;
+   this->w = WIDGET_WIDTH;  this->h = WIDGET_HEIGHT;
+   visible = false;
+   focus = false;
+   this->father = father;
+
+   old_screen = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                      this->w, this->h,
+                      screen->format->BitsPerPixel,
+                      screen->format->Rmask,
+                      screen->format->Gmask,
+                      screen->format->Bmask,
+                      screen->format->Amask);
+
+   widget = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                  this->w, this->h,
+                  screen->format->BitsPerPixel,
+                  screen->format->Rmask,
+                  screen->format->Gmask,
+                  screen->format->Bmask,
+                  screen->format->Amask);
+ }
+
+ Widget::Widget(const uint w, const uint h, SDL_Surface * screen, Widget* father)
+ {
+   this->screen = screen;
+   this->x = 0;  this->y = 0;
+   this->w = w;  this->h = h;
+   visible = false;
+   Widget::focus = false;
+   this->father = father;
+   Focus globfocus();
+
+   old_screen = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                      this->w, this->h,
+                      screen->format->BitsPerPixel,
+                      screen->format->Rmask,
+                      screen->format->Gmask,
+                      screen->format->Bmask,
+                      screen->format->Amask);
+
+   widget = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                  this->w, this->h,
+                  screen->format->BitsPerPixel,
+                  screen->format->Rmask,
+                  screen->format->Gmask,
+                  screen->format->Bmask,
+                  screen->format->Amask);
+ }
+
+ Widget::Widget(const uint x, const uint y, const uint w, const uint h,
+                 SDL_Surface * screen, Widget* father)
+ {
+   this->screen = screen;
+   this->x = x;  this->y = y;
+   this->w = w;  this->h = h;
+   visible = false;
+   Widget::focus = false;
+   this->father = father;
+   Focus globfocus();
+
+   old_screen = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                      this->w, this->h,
+                      screen->format->BitsPerPixel,
+                      screen->format->Rmask,
+                      screen->format->Gmask,
+                      screen->format->Bmask,
+                      screen->format->Amask);
+
+   widget = SDL_CreateRGBSurface (SDL_HWSURFACE,
+                  this->w, this->h,
+                  screen->format->BitsPerPixel,
+                  screen->format->Rmask,
+                  screen->format->Gmask,
+                  screen->format->Bmask,
+                  screen->format->Amask);
+ }
+
+ Widget::~Widget()
+ {
+     if(hasfocus())
+     {
+         globfocus.set_focus(NULL);
+     }
+     SDL_FreeSurface(widget);
+     SDL_FreeSurface(old_screen);
+ }
+
+ void Widget::erase()
+ {
+   SDL_Rect r = {get_wx(), get_wy(), w, h};
+   SDL_BlitSurface(old_screen, NULL, screen, &r);
+   SDL_Flip(screen);
+ }
+
+ ushort Widget::inside(const uint mousex, const uint mousey)
+ {
+   const uint wx = get_wx();
+   const uint wy = get_wy();
+   return((mousex >= wx) && (mousex <= (wx + w)) && (mousey >= wy) && (mousey <= (wy + h)));
+ }
+
+ ushort Widget::mousebuttondown(const uint mousex, const uint mousey)
+ {
+     if(inside(mousex, mousey))
+     {
+         globfocus.set_focus(this);
+         return 1;
+     }
+     return 0;
+ }
+
+ void Widget::gainfocus()
+ {
+     focus=true;
+ }
+
+ void Widget::losefocus()
+ {
+     focus=false;
+ }
+
+ bool Widget::hasfocus()
+ {
+     return(focus);
+ }
+
+ Focus& Widget::get_focus()
+ { 
+     return globfocus;
+ }
+
+ uint Widget::get_x()
+ {
+   return x;
+ }
+
+ uint Widget::get_wx()
+ {
+     if(father != NULL)
+     {
+         return((father->get_wx()) + x);
+     }
+     else
+     {
+         return(x);
+     }
+ }
+
+ void Widget::set_x(const uint x)
+ {
+     this->x = x;
+ }
+
+ uint Widget::get_y()
+ {
+   return y;
+ }
+
+ void Widget::set_y(const uint y)
+ {
+     this->y = y;
+ }
+
+ uint Widget::get_wy()
+ {
+    if(father != NULL)
+     {
+         return((father->get_wy()) + y);
+     }
+     else
+     {
+         return(y);
+     } 
+ }
+
+ uint Widget::get_height()
+ {
+   return h;
+ }
+
+ void Widget::set_height(const uint height)
+ {
+     h = height;
+ }
+
+ uint Widget::get_width()
+ {
+   return w;
+ }
+
+ void Widget::set_width(const uint width)
+ {
+     w = width;
+ }
---
+++ bb5/visu2d/Widget.hh
@@ -0,0 +1,113 @@
+ /*
+  ** TowBowlTactics, an adaptation of the tabletop game Blood Bowl.
+  ** 
+  ** Copyright (C) 2006 The TBT Team.
+  ** 
+  ** This program is free software; you can redistribute it and/or
+  ** modify it under the terms of the GNU General Public License
+  ** as published by the Free Software Foundation; either version 2
+  ** of the License, or (at your option) any later version.
+  ** 
+  ** The complete GNU General Public Licence Notice can be found as the
+  ** `NOTICE' file in the root directory.
+  ** 
+  ** The TBT Team consists of people listed in the `AUTHORS' file.
+  */
+
+ #ifndef WIDGET_H
+ #  define WIDGET_H
+ #include "Global.hh"
+ #include "Focus.hh"
+
+ #define WIDGET_HEIGHT 30
+ #define WIDGET_WIDTH 150
+ class Focus;
+
+ class Widget
+ {
+ protected:
+   // screen is the father and old_screen a copy of screen
+   SDL_Surface *screen, *old_screen, *widget;
+   Widget* father; // father widget
+   uint x, y; // in the father coordinate system
+   uint w, h; // size
+   bool visible;
+   bool focus;
+   static Focus globfocus;
+
+ public:
+     Widget(SDL_Surface* screen, Widget* father = NULL);
+     Widget(const uint w, const uint h, SDL_Surface* screen, Widget* father = NULL);
+     Widget(const uint x, const uint y, const uint w, const uint h, SDL_Surface* screen, Widget* father=NULL);
+    ~Widget();
+
+   // Draw
+   virtual void draw() = 0;
+   virtual void refresh() = 0;
+   void erase();
+
+   // Mouse
+   virtual void mousemotion(const uint mousex, const uint mousey) = 0;
+   ushort inside(const uint mousex, const uint mousey);
+   virtual ushort mousebuttondown(const uint mousex, const uint mousey);
+
+   // Focus
+   virtual void gainfocus();
+   virtual void losefocus();
+   virtual bool hasfocus();
+   static Focus& get_focus(); 
+
+   // Geometry
+   //! @brief Method to get the x coordinate of the widget
+   //!  in the father coordinate system
+   //!
+   //! @return the x coordinate
+   uint get_x();
+
+   //! @brief Method to get the x coordinate of the widget
+   //!  in the window coordinate system
+   //!
+   //! @return the x coordinate
+   uint get_wx();
+
+   //! @brief Method to set the x coordinate of the widget
+   //!  in the father coordinate system
+   void set_x(const uint x);
+
+   //! @brief Method to get the y coordinate of the widget
+   //!  in the father coordinate system
+   //!
+   //! @return the y coordinate
+   uint get_y();
+
+   //! @brief Method to get the y coordinate of the widget
+   //!  in the window coordinate system
+   //!
+   //! @return the y coordinate
+   uint get_wy();
+
+   //! @brief Method to set the y coordinate of the widget
+   //!  in the father coordinate system
+   void set_y(const uint y);
+
+   //! @brief Method to get the width the widget
+   //!
+   //! @return the width of the widget
+   uint get_width();
+
+   void set_width(const uint width);
+
+   //! @brief Method to get the height of the widget
+   //!
+   //! @return the height of the wdget
+   uint get_height();
+
+   void set_height(const uint height);
+
+     // Txt .. for textuals widgets
+   virtual void add_char(const char c){};
+   virtual void delete_char(){};
+   virtual ushort get_index(){return 0;}; 
+   virtual void set_index(const ushort i){};
+ };
+ #endif
