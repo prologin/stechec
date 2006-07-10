8591c67232918456fe6c1ff32672b23a137cd049
victor@14d9d42f-b097-442a-9da6-5b64999cc983 <victor@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-07-10 19:11:45 +0000

    Update, now compile (from poltuiu).

--- bb5/visu2d/main.cc
+++ bb5/visu2d/main.cc
@@ -18,6 +18,9 @@
 #include "Global.hh"
 #include "DialogBox.hh"
 #include "GuiError.hh"
+#include "Label.hh"
+#include "Entry.hh"
+#include "LabelEntry.hh"
 #include "Menu.hh"
 
 using namespace std;
@@ -31,76 +34,85 @@ const unsigned int SCREEN_BPP = 32;
 SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
 
-const string icon = ADD_IMG_PATH("general/tbt.ico");
-const string file = ADD_IMG_PATH("screens/title_bg.jpg");
+const string icon = ADD_IMG_PATH ("general/tbt.ico");
+const string file = ADD_IMG_PATH ("screens/title_bg.jpg");
 
-vector<DialogBox*> list_box;
+vector <DialogBox*> list_box;
 Menu* menu;
 
+//Replace by real player info
+struct Player
+{
+  string playername;
+  string ip;
+} player;
+
 // Parse xml configuration file.
-static bool parse_config(const char* opt_file, xml::XMLConfig& cfg)
+static bool parse_config (const char *opt_file, xml::XMLConfig & cfg)
 {
   try
   {
     if (opt_file != NULL)
-      cfg.parse(opt_file);
+      cfg.parse (opt_file);
     else
-      cfg.parse("");
+      cfg.parse ("");
   }
-  catch (const xml::XMLError&)
+  catch (const xml::XMLError &)
   {
-    ERR("Sorry, I can't go further without a working configuration file...");
+    ERR ("Sorry, I can't go further without a working configuration file...");
     return false;
   }
   return true;
 }
 
-// Set some basic settings based on XML config file.
-static void set_opt(const char* section, xml::XMLConfig& cfg, Log& log)
+    // Set some basic settings based on XML config file.
+static void set_opt(const char *section, xml::XMLConfig & cfg, Log & log)
 {
-  cfg.switchSection(section);
-  log.setVerboseLevel(cfg.getAttr<int>("debug", "verbose"));
-  log.setPrintLoc(cfg.getAttr<bool>("debug", "printloc"));
+  cfg.switchSection (section);
+  log.setVerboseLevel (cfg.getAttr <int>("debug", "verbose"));
+  log.setPrintLoc (cfg.getAttr <bool> ("debug", "printloc"));
 }
 
 // To clean and exit
 void Exit(void)
 {
       // clean 
-  SDL_FreeSurface(background);
-  SDL_FreeSurface(screen);
+  SDL_FreeSurface (background);
+  SDL_FreeSurface (screen);
       //clean list
-  list_box.clear();
+  list_box.clear ();
       //quit SDL
-  SDL_Quit();
-  LOG1("Bye bye");
+  SDL_Quit ();
+  LOG1 ("Bye bye");
 }
 
-// Init SDL
-bool initSDL(void)
+    // Init SDL
+bool initSDL (void)
 {
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
   {
-    ERR("Error in initialisation of the SDL : "<<SDL_GetError());
+    ERR("Error in initialisation of the SDL : " << SDL_GetError ());
     return false;
   }
-  
-  SDL_WM_SetIcon(IMG_Load(icon.c_str()),NULL);
-  screen = SDL_SetVideoMode(SCREEN_WIDTH_MIN, SCREEN_HEIGHT_MIN, SCREEN_BPP, SDL_SWSURFACE);
-  
-  if (screen == NULL)
+
+  SDL_WM_SetIcon(IMG_Load (icon.c_str ()), NULL);
+  screen = SDL_SetVideoMode (SCREEN_WIDTH_MIN, SCREEN_HEIGHT_MIN, SCREEN_BPP, SDL_SWSURFACE);
+
+  if(screen == NULL)
   {
-    ERR("Impossible to activate graphic mode : "<<SDL_GetError());
+    ERR("Impossible to activate graphic mode : " << SDL_GetError ());
     return false;
   }
-  
+
   SDL_WM_SetCaption("TBT", NULL);
       // Mouse an keyboard are confined to the application window
   SDL_WM_GrabInput(SDL_GRAB_OFF);
+      //Enables Unicode keyboard translation.
+  SDL_EnableUNICODE(1);
   return true;
 }
 
-void addDialog(DialogBox* box)
+void addDialog(DialogBox * box)
 {
   list_box.push_back(box);
 }
@@ -109,42 +121,93 @@ void MainLoop(void)
 {
   SDL_Event event;
   bool quit = false;
-  
   while (SDL_WaitEvent(&event) && quit == false)
-  {  /* Loop until there are no events left on the queue */
-    switch (event.type)
+  {				/* Loop until there are no events left on the queue */
+    switch(event.type)
     {
       case SDL_QUIT:
         quit = true;
         break;
       case SDL_KEYDOWN:
-        switch (event.key.keysym.sym)
-        { 
-          case SDLK_RETURN:
+      {
+        SDL_keysym* keysym = &event.key.keysym;
+        Widget* focus = (Widget::get_focus()).get_focus();
+        if(focus != NULL)
+        {
+          if(keysym->unicode<127 and keysym->unicode>=32)
           {
-                // hmm seems KMOD_MODE is Alt Gr key !
-            unsigned short ModState = (SDL_GetModState() & 0x4fff);
-            if (ModState == KMOD_LALT || ModState == KMOD_MODE)
-            {
-              SDL_WM_ToggleFullScreen(screen);
-            }
-            break; 
+                // ASCII character
+            char ch = (keysym->unicode & 0x7F);
+            ushort index=focus->get_index();
+            focus->add_char(ch);
+            focus->set_index(++index);
+            focus->refresh();   
           }
-          default:
-            break;
+          else
+          {
+            switch(keysym->sym)
+            {
+              case SDLK_DELETE:
+              {
+                focus->delete_char();
+                focus->refresh();
+                break;
+              }
+              case SDLK_BACKSPACE:
+              {
+                ushort index = focus->get_index();
+                if(index > 0)
+                {
+                  focus->set_index(--index);
+                  focus->delete_char();
+                  focus->refresh();
+                }
+                break;
+              }
+              case SDLK_LEFT:
+              {
+                ushort index=focus->get_index();
+                if(index > 0)
+                {
+                  focus->set_index(--index);
+                  focus->refresh();
+                }
+                break;
+              }
+              case SDLK_RIGHT:
+              {
+                ushort index=focus->get_index();
+                focus->set_index(++index);
+                focus->refresh(); 
+                break;
+              }
+              case SDLK_RETURN:
+              {
+                    // hmm seems KMOD_MODE is Alt Gr key !
+                    // we need to ignore 0x1000 (KMOD_NUM) and 0x2000 (KMOD_CAPS)
+                ushort ModState = (SDL_GetModState() & 0x4FFF);
+                if (ModState == KMOD_LALT || ModState == KMOD_MODE)
+                {
+                  SDL_WM_ToggleFullScreen(screen);
+                }
+                break;
+              }
+            }
+          }
         }
         break;
+      }
       case SDL_MOUSEBUTTONDOWN:
         if (event.button.button == SDL_BUTTON_LEFT)
         {
               // All DialogBox
-          for (vector<DialogBox*>::iterator box = list_box.begin();
-               box != list_box.end(); ++box)
+          for (vector <DialogBox*>::iterator box = list_box.begin ();
+               box != list_box.end (); ++box)
           {
-            int res=(*box)->mousebuttondown(event.button.x, event.button.y);
-            if(res > 0)
+            int res = (*box)->mousebuttondown(event.button.x, event.button.y);
+            if (res > 0)
             {
-              switch(res)
+              switch (res)
               {
                 case CLOSE:
                   delete(*box);
@@ -155,8 +218,8 @@ void MainLoop(void)
             }
           }
               // Menu
-          int res=menu->mousebuttondown(event.button.x, event.button.y);
-          if(res > 0)
+          int res = menu->mousebuttondown(event.button.x, event.button.y);
+          if (res > 0)
           {
             switch(res)
             {
@@ -167,62 +230,68 @@ void MainLoop(void)
               }
               case BACK:
               {
-                menu -> erase();
+                menu->erase();
                 delete menu;
                 try
                 {
-                  menu=new Menu(5, 5, 5, screen, MENU_WIDTH, "MENU");
+                  menu = new Menu(5, 5, screen, MENU_WIDTH, "MENU");
                 }
-                catch(GUIError& error)
+                catch(GUIError & error)
                 {
-                  ERR("Error in creation of Menu : "<<error.getMessage());
+                  ERR ("Error in creation of Menu : " << error.getMessage ());
                   quit = true;
                   break;
                 }
-                menu->draw();
-                menu->addButton("NETWORK GAME", NETWORK);
-                menu->addButton("LOCAL GAME", STANDALONE);
-                menu->addButton("SERVER", SERVER);
-                menu->addButton("OPTIONS", OPTIONS);
-                menu->addButton("EXIT", EXIT);
-                menu->refresh();
+                menu->draw ();
+                menu->addWidget(new TextButton(0, 0, MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "NETWORK GAME", NETWORK));
+                menu->addWidget(new TextButton(0, 0, MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "LOCAL GAME", STANDALONE));
+                menu->addWidget(new TextButton(0, 0, MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "SERVER", SERVER));
+                menu->addWidget(new TextButton(0, 0, MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "OPTIONS", OPTIONS));
+                menu->addWidget(new TextButton(0, 0, MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "EXIT", EXIT));
+                menu->refresh ();
                 break;
               }
               case NETWORK:
               {
-                menu -> erase();
+                menu->erase();
                 delete menu;
                 try
                 {
-                  menu=new Menu(5, 5, 5, screen, 250, "NETWORK GAME");
+                  menu = new Menu(5, 5, screen, (2 * MENU_WIDGET_WIDTH) + 15, MENU_WIDGET_HEIGHT + 10,"NETWORK GAME");
                 }
-                catch(GUIError& error)
+                catch (GUIError & error)
                 {
-                  ERR("Error in creation of Menu : "<<error.getMessage());
+                  ERR ("Error in creation of Menu : " << error.getMessage ());
                   quit = true;
                   break;
                 }
                 menu->draw();
-                menu->addButton("Back", BACK);
+                menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "Back", BACK),
+                                (menu->getwidth() - ((2 * MENU_WIDGET_WIDTH) + menu->getpadx())) / 2, 
+                                (menu->getheight() - MENU_WIDGET_HEIGHT - menu->getpady()));
+                menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "Ok", OK),
+                                (menu->getwidth() + menu->getpadx()) / 2, 
+                                (menu->getheight() - MENU_WIDGET_HEIGHT - menu->getpady()));
                 menu->refresh();
                 break;
               }
               case OPTIONS:
               {
-                menu -> erase();
+                menu->erase();
                 delete menu;
                 try
                 {
-                  menu=new Menu(5, 5, 5, screen, MENU_WIDTH , "OPTIONS");
+                  menu = new Menu(5, 5, screen, MENU_WIDTH, "OPTIONS");
                 }
-                catch(GUIError& error)
+                catch(GUIError & error)
                 {
-                  ERR("Error in creation of Menu : "<<error.getMessage());
+                  ERR("Error in creation of the menu : " << error.getMessage ());
                   quit = true;
                   break;
                 }
                 menu->draw();
-                menu->addButton("Back", BACK);
+                menu->addWidget(new LabelEntry(0, 0, 2 * MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "Nom du joueur", &player.playername));
+                menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "Back", BACK));
                 menu->refresh();
                 break;
               }
@@ -242,10 +311,10 @@ void MainLoop(void)
       case SDL_MOUSEBUTTONUP:
         if (event.button.button == SDL_BUTTON_LEFT)
         {
-          for (vector<DialogBox*>::iterator box = list_box.begin();
+          for (vector <DialogBox*>::iterator box = list_box.begin();
                box != list_box.end(); ++box)
           {
-            if((*box)->mousebuttonup(event.button.x, event.button.y) == 1)
+            if ((*box)->mousebuttonup() == 1)
               break;
           }
         }
@@ -256,8 +325,8 @@ void MainLoop(void)
         for (vector<DialogBox*>::iterator box = list_box.begin();
              box != list_box.end(); ++box)
         {
-          if((*box)->mousemotion(event.button.x, event.button.y, 
-                                 event.motion.xrel, event.motion.yrel) == 1)
+          if ((*box)->mousemotion(event.button.x, event.button.y,
+                                  event.motion.xrel, event.motion.yrel) == 1)
             break;
         }
       default:
@@ -266,12 +335,12 @@ void MainLoop(void)
   }
 }
 
-bool initBackground(void)
+bool initBackground (void)
 {
-  background = LoadImage(file,0);
+  background = LoadImage (file, 0);
   if (background == NULL)
   {
-    ERR("Error in initialisation of the background : "<<SDL_GetError());
+    ERR("Error in initialisation of the background : " << SDL_GetError());
     return false;
   }
   SDL_Rect *srect = NULL;
@@ -288,27 +357,27 @@ bool initBackground(void)
     srect->y = (sh - bgh) / 2;
   }
       //Blit the surface
-  if (SDL_BlitSurface(background, NULL, screen, srect) != 0)
+  if (SDL_BlitSurface (background, NULL, screen, srect) != 0)
   {
-    ERR("Error in initialisation of the background : "<<SDL_GetError());
+    ERR("Error in initialisation of the background : " << SDL_GetError());
     return false;
   }
-  SDL_Flip(screen);
-  delete(srect);
+  SDL_Flip (screen);
+  delete (srect);
   return true;
 }
 
-bool initTTF(void)
+bool initTTF (void)
 {
-  if (TTF_Init() < 0)
+  if (TTF_Init () < 0)
   {
-    ERR("Error in intialisation of the SDL_TTF : "<<SDL_GetError());
+    ERR("Error in intialisation of the SDL_TTF : " << SDL_GetError());
     return false;
   }
   return true;
 }
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
   Log log_client(5);
   xml::XMLConfig cfg;
@@ -316,49 +385,50 @@ int main(int argc, char* argv[])
   if (initSDL() == false)
     return -1;
   LOG1("Init SDL .. ok");
-  if (initTTF() == false )
+  if (initTTF() == false)
     return -1;
   LOG1("Init SDL_ttf .. ok");
   if (initBackground() == false)
     return -1;
   LOG1("Init background .. ok");
-  
+
       //display a menu to test it
   try
   {
-    menu=new Menu(5, 5, 5, screen, MENU_WIDTH, "MENU");
+    menu = new Menu (5, 5, screen, MENU_WIDTH, "MENU");
   }
-  catch(GUIError& error)
+  catch (GUIError & error)
   {
-    ERR("Error in creation of Menu : "<<error.getMessage());
+    ERR ("Error in creation of Menu : " << error.getMessage ());
     return -1;
   }
-  menu->draw();
-  menu->addButton("NETWORK GAME", NETWORK);
-  menu->addButton("LOCAL GAME", STANDALONE);
-  menu->addButton("SERVER", SERVER);
-  menu->addButton("OPTIONS", OPTIONS);
-  menu->addButton("EXIT", EXIT);
-  menu->refresh();
+  menu->draw ();
+  menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "NETWORK GAME", NETWORK));
+  menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "LOCAL GAME", STANDALONE));
+  menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "SERVER", SERVER));
+  menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "OPTIONS", OPTIONS));
+  menu->addWidget(new TextButton(MENU_WIDGET_WIDTH, MENU_WIDGET_HEIGHT, screen, menu, "EXIT", EXIT));
+  menu->refresh ();
 
       // display a DialogBox to test it
-  DInfo* infobox;
+  DInfo *infobox;
   try
   {
-    infobox = new DInfo(screen, "Bienvenue dans TBT\n\nAttention !!\nce programe est\nen développement\n");
+    infobox = new DInfo(screen,
+                        "Bienvenue dans TBT\n\nAttention !!\nce programe est\nen développement\n");
   }
-  catch(GUIError& error)
+  catch(GUIError & error)
   {
-    ERR("Error in creation of a DialogBox : "<<error.getMessage());
+    ERR("Error in creation of a DialogBox : " << error.getMessage());
     return -1;
   }
-
+  
   addDialog(infobox);
   infobox->draw();
-
+  
       // event loop
   MainLoop();
-  
+
       // .. and exit
   Exit();
   return EXIT_SUCCESS;
