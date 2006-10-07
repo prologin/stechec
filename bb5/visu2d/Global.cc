97902551cea857d69d6da1714ae222ae7c304fb0
poltuiu@14d9d42f-b097-442a-9da6-5b64999cc983 <poltuiu@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-10-07 22:33:03 +0000

    Correct warning using TTF_GetError

--- bb5/visu2d/Global.cc
+++ bb5/visu2d/Global.cc
@@ -55,7 +55,7 @@ void PrintStrings(SDL_Surface *dest, TTF_Font *font, string str,
     height = (vLines.size()-1) * lineSkip + height; 
     temp = TTF_RenderText_Solid(font, tmpstr.c_str(), textColor);
     if (temp == NULL)
-      WARN("FAILED to render `%1` : %2", tmpstr, (int*)font);
+      WARN("FAILED to render text : %1", TTF_GetError());
     
     SDL_Rect r = {rc.x + (rc.w - width) / 2, rc.y + (rc.h-height) / 2 + i * lineSkip, rc.w, rc.h};
 
