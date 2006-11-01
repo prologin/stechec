9671e8666aa3ac2c7ed77fa64ca075d9cc84ca97
tuxrouge@14d9d42f-b097-442a-9da6-5b64999cc983 <tuxrouge@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-11-01 12:06:29 +0000

    add test's files (pass)

---
+++ bb5/rules_test/pass_1.cmd
@@ -0,0 +1,18 @@
+wait
+cheat 5 
+declare blitz 0
+block 0 0
+accept
+push 2
+follow
+cheat 6
+move 0 14 0
+move 0 17 0
+cheat 6 6
+declare pass 10
+move 10 9 1
+pass 10 17 0
+print BEGIN DIFF
+print global
+print END DIFF
+quit
