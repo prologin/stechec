c7a18df6e1a7b6ad7057953461208fa03dbc0bcf
victor@14d9d42f-b097-442a-9da6-5b64999cc983 <victor@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-11-01 19:01:15 +0000

    Make comments clearer.

--- bb5/rules_test/pickupf_1.cmd
+++ bb5/rules_test/pickupf_1.cmd
@@ -1,7 +1,8 @@
 # a ce moment, on est en debut de jeu/kickoff
-wait
 
-# a ce moment, on est au debut tour1 joueur1
+wait
+# a ce moment, tour1 joueur1
+
 # effectue toutes les actions qu'on veut...
 cheat 2 5
 declare move 10
@@ -9,15 +10,14 @@ move 10 6 8
 accept
 # ce dernier 'accepte' fait qu'on ne recupere pas la balle - turnover.
 # a ce moment, on passe en tour1 joueur2.
+# c'est comme si on avait fait 'end'.
 
-# ici, pour que les 'print' aient le temps de s'afficher, il faut continuer
-# a jouer 1 tour 'dans le vent'
+wait
+# a ce moment, tour2 joueur1
+
+# on affiche les infos et on se casse.
 print BEGIN DIFF
 print us 10
 print END DIFF
 
-# toujours tour1 joueur2.
-wait
-
-# a ce moment, tour2 joueur1. on peut quitter maintenat
 quit
--- bb5/rules_test/pickupf_2.cmd
+++ bb5/rules_test/pickupf_2.cmd
@@ -1,7 +1,7 @@
 # le premier wait est fait pour attendre le debut du kickoff
 wait
-
 # a ce moment, on est dans la phase kick-off
+
 cheat 1 1 1 1
 kickoff 10 8
 
@@ -13,15 +13,15 @@ print END DIFF
 # a ce moment, vu qu'on a fait 'kickoff', on passe en phase tour1 joueur1 
 # ce wait sert a attendre patiemment le tour1 joueur2, sinon ce client quitte
 # (plus de commande), et le test foire
-wait
 
+wait
 # a ce moment, tour1 joueur2
 
 # termine son tour immediatement, on veut rien faire.
 end
 
-# a ce moment, tour2 joueur1. le joueur1 va quitter la partie, et
-# nous faire quitter en meme temps.
+# a ce moment, tour2 joueur1. le joueur1 s'occupe de quitter la partie, 
+# il suffit d'attendre (un hypothetique tour2 joueur2).
 wait
 
 # - never reached -
