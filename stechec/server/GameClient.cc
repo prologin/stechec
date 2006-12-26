8a83f12372cdf3ac837e0a8dd197d5cefa32c067
victor@14d9d42f-b097-442a-9da6-5b64999cc983 <victor@14d9d42f-b097-442a-9da6-5b64999cc983>
2006-12-26 20:10:33 +0000

    Fix missing file.

--- stechec/server/GameClient.cc
+++ stechec/server/GameClient.cc
@@ -1,21 +1,17 @@
 /*
-** TowBowlTactics, an adaptation of the tabletop game Blood Bowl.
-** 
-** Copyright (C) 2006 The TBT Team.
-** 
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the GNU General Public License
-** as published by the Free Software Foundation; either version 2
-** of the License, or (at your option) any later version.
-** 
+** Stechec project is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
 ** The complete GNU General Public Licence Notice can be found as the
 ** `NOTICE' file in the root directory.
-** 
-** The TBT Team consists of people listed in the `AUTHORS' file.
+**
+** Copyright (C) 2006 Prologin
 */
 
 #include "BaseSRules.hh"
-#include "game_hosting.hh"
+#include "GameHosting.hh"
 #include "GameClient.hh"
 
 BEGIN_NS(server);
