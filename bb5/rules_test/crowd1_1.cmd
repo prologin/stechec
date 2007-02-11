094f6b5907658310c6e60202439cb9c06eabc7bf
fekmon@14d9d42f-b097-442a-9da6-5b64999cc983 <fekmon@14d9d42f-b097-442a-9da6-5b64999cc983>
2007-02-11 13:56:37 +0000

    Add crowd1 rules test (push a player in the furious crowd of supporters). (XFAIL) pass' tests pass.

---
+++ bb5/rules_test/crowd1_1.cmd
@@ -0,0 +1,17 @@
+wait
+# Give ball to player 10
+giveBall 10
+
+# Turn 1.
+
+# Move player 0 near the crowd.
+cheat 6
+declare move 0
+move 0 13 0
+
+# Do nothing else.
+end
+
+wait
+# Turn 2, not reached.
+print This should not be printed, in any case.
---
+++ bb5/rules_test/crowd1_2.cmd
@@ -0,0 +1,26 @@
+wait
+# Kickoff.
+# Ball is scattered off-field.
+cheat 1 1
+kickoff 0 0
+
+wait
+# Turn 1.
+
+# Push player 0 off the pitch.
+cheat 3 
+cheat 1 1
+declare block 0
+block 0 0
+accept
+
+# A bug happened here, causing the server to crash.
+
+block follow
+
+# Player 0 should be stunned, then placed in reserve.
+print BEGIN DIFF
+print them 0
+print END DIFF
+
+quit
