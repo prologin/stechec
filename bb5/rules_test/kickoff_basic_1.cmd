c4f2d2bd9392d355eb7a4a6686e72ea601903bf5
fekmon@14d9d42f-b097-442a-9da6-5b64999cc983 <fekmon@14d9d42f-b097-442a-9da6-5b64999cc983>
2007-04-29 15:25:53 +0000

    BB5: Fixes kick-off rules tests.

--- bb5/rules_test/kickoff_basic_1.cmd
+++ bb5/rules_test/kickoff_basic_1.cmd
@@ -1,11 +1,8 @@
 wait
-#Team placement.
+# Team placement.
 place end
 
 wait
-# Give ball to player 10.
-giveBall 10
-
 # Turn 1.
 
 print BEGIN DIFF
---
+++ bb5/rules_test/kickoff_scatter_scrimmage_1.cmd
@@ -0,0 +1,16 @@
+wait
+# Team placement.
+place end
+
+wait
+# Give ball to player 10.
+giveBall 10
+
+wait
+# Turn 1.
+
+print BEGIN DIFF
+print global
+print END DIFF
+
+quit
