6590f56950e395fac6c63a7af3987d22a79b2e3d
fekmon@14d9d42f-b097-442a-9da6-5b64999cc983 <fekmon@14d9d42f-b097-442a-9da6-5b64999cc983>
2007-05-06 19:53:43 +0000

    Add missing SActionHandler files to the trunk.

---
+++ bb5/arbiter/SActionHandler.cc
@@ -0,0 +1,263 @@
+/*
+** TowBowlTactics, an adaptation of the tabletop game Blood Bowl.
+** 
+** Copyright (C) 2006, 2007 The TBT Team.
+** 
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+** 
+** The complete GNU General Public Licence Notice can be found as the
+** `NOTICE' file in the root directory.
+** 
+** The TBT Team consists of people listed in the `AUTHORS' file.
+*/
+
+#include "SActionHandler.hh"
+#include "SRules.hh"
+
+SActionHandler::SActionHandler(SRules* r)
+  : r_(r),
+  d_(r->getDice())
+{
+}
+
+SActionHandler::~SActionHandler()
+{
+  //FIXME: to do.
+}
+
+bool SActionHandler::isEmpty() const
+{
+  return eal_.empty();
+}
+
+SPlayer* SActionHandler::getPlayer() const
+{
+  if (eal_.empty())
+    {
+      return NULL;
+    }
+  else
+    {
+      return eal_.back().player;
+    }
+}
+
+enum eRoll SActionHandler::getRollType() const
+{
+  if (eal_.empty())
+    {
+      return R_NONE;
+    }
+  else
+    {
+      return eal_.back().roll_type;
+    }
+}
+
+inline void SActionHandler::put(ElementaryAction ea)
+{
+  eal_.push_back(ea);
+}
+
+void SActionHandler::putArmourRoll(SPlayer* p, int av_mod, int inj_mod)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_ARMOUR;
+  ea.player = p;
+  ea.modifier = av_mod;
+  ea.next_mod = inj_mod;
+  put(ea);
+}
+
+void SActionHandler::putBallBounce()
+{
+  ElementaryAction ea;
+  ea.type = EA_BALL;
+  ea.roll_type = R_NONE;
+  ea.player = NULL;
+  put(ea);
+}
+
+void SActionHandler::putBlockDiceChoice(SPlayer* p)
+{
+  ElementaryAction ea;
+  ea.type = EA_BLOCKDICE;
+  ea.roll_type = R_NONE;
+  ea.player = p;
+  put(ea);
+}
+
+void SActionHandler::putBlockFollowChoice(SPlayer* p)
+{
+  ElementaryAction ea;
+  ea.type = EA_BLOCKFOLLOW;
+  ea.roll_type = R_NONE;
+  ea.player = p;
+  put(ea);
+}
+
+void SActionHandler::putBlockPushChoice(SPlayer* p)
+{
+  ElementaryAction ea;
+  ea.type = EA_BLOCKPUSH;
+  ea.roll_type = R_NONE;
+  ea.player = p;
+  put(ea);
+}
+
+void SActionHandler::putBlockRoll(SPlayer* p)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_BLOCK;
+  ea.player = p;
+  put(ea);
+}
+
+void SActionHandler::putCatchBallRoll(SPlayer* p, bool success)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_CATCH;
+  ea.player = p;
+  ea.success = success;
+  put(ea);
+}
+
+void SActionHandler::putDodgeRoll(SPlayer* p, bool success)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_DODGE;
+  ea.player = p;
+  ea.success = success;
+  put(ea);
+}
+
+void SActionHandler::putMove(SPlayer* p, Position aim)
+{
+  ElementaryAction ea;
+  ea.type = EA_MOVE;
+  ea.roll_type = R_NONE;
+  ea.player = p;
+  ea.aim_position = aim;
+  put(ea);
+}
+
+void SActionHandler::putPickUpRoll(SPlayer* p, bool success)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_PICKUP;
+  ea.player = p;
+  ea.success = success;
+  put(ea);
+}
+
+void SActionHandler::putPushInTheCrowd(SPlayer* p)
+{
+  ElementaryAction ea;
+  ea.type = EA_PUSHOFFFIELD;
+  ea.roll_type = R_NONE;
+  ea.player = p;
+  put(ea);
+}
+
+void SActionHandler::putStandUpRoll(SPlayer* p, bool success)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_STANDUP;
+  ea.player = p;
+  ea.success = success;
+  put(ea);
+}
+
+void SActionHandler::putThrowRoll(SPlayer* p, int success)
+{
+  ElementaryAction ea;
+  ea.type = EA_ROLL;
+  ea.roll_type = R_THROW;
+  ea.player = p;
+  ea.success = success;
+  put(ea);
+}
+
+void SActionHandler::process(bool reroll, int choice)
+{
+  ElementaryAction ea;
+  if (eal_.empty())
+    {
+      if (r_->isTurnover() || r_->getState() == GS_KICKOFF)
+        {
+          r_->nextTurn();
+        }
+      return;
+    }
+  ea = eal_.back();
+  eal_.pop_back();
+  switch (ea.type)
+    {
+      case EA_BALL:
+        r_->getBall()->bounce();
+        break;
+      case EA_BLOCKDICE:
+        ea.player->resolveBlockDice(choice);
+        break;
+      case EA_BLOCKFOLLOW:
+        ea.player->blockFollow(reroll);
+        break;
+      case EA_BLOCKPUSH:
+        ea.player->resolveBlockPush(choice);
+        break;
+      case EA_MOVE:
+        if (r_->isTurnover())
+          process();
+        else
+          ea.player->tryMove(ea.aim_position);
+        break;
+      case EA_PUSHOFFFIELD:
+        ea.player->bePushedInTheCrowd();
+        break;
+      case EA_ROLL:
+        switch (ea.roll_type)
+          {
+            case R_ARMOUR:
+              ea.player->checkArmour(ea.modifier, ea.next_mod);
+              break;
+            case R_BLOCK:
+              ea.player->chooseBlockDice(reroll);
+              break;
+            case R_CATCH:
+              ea.player->finishCatchBall(reroll, ea.success);
+              break;
+            case R_DODGE:
+              ea.player->finishDodge(reroll, ea.success);
+              break;
+            case R_INJURY:
+              WARN("Injury roll should never be put off.");
+              break;
+            case R_PICKUP:
+              ea.player->finishPickUp(reroll, ea.success);
+              break;
+            case R_STANDUP:
+              ea.player->finishStandUp(reroll, ea.success);
+              break;
+            case R_THROW:
+              ea.player->finishThrow(reroll, ea.success);
+              break;
+            default:
+              WARN("Unknown roll (%1) for elementary action (%2).", ea.roll_type, ea.type);
+              break;
+          }
+        break;
+      default:
+        WARN("Unknown elementary action (%1).", ea.type);
+        return;
+    }
+}
+
---
+++ bb5/arbiter/SActionHandler.hh
@@ -0,0 +1,97 @@
+/*
+** TowBowlTactics, an adaptation of the tabletop game Blood Bowl.
+** 
+** Copyright (C) 2006, 2007 The TBT Team.
+** 
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+** 
+** The complete GNU General Public Licence Notice can be found as the
+** `NOTICE' file in the root directory.
+** 
+** The TBT Team consists of people listed in the `AUTHORS' file.
+*/
+
+#ifndef SACTIONHANDLER_HH_
+# define SACTIONHANDLER_HH_
+
+# include "Dice.hh"
+
+class SRules;
+class SPlayer;
+
+/*!
+** @brief Elementary action handler for the server.
+**
+** It memorizes the sequence of pending elementary actions.
+**
+** You will have only 1 instance of this class at runtime,
+** handled by the SRules and SPlayer classes.
+*/
+class SActionHandler
+{
+public:
+
+  SActionHandler(SRules* r);
+  virtual ~SActionHandler();
+
+  bool isEmpty() const;
+  SPlayer* getPlayer() const;
+  enum eRoll getRollType() const;
+
+  //! @brief Adds an elementary action to the stack.
+  void putArmourRoll(SPlayer* p, int av_mod = 0, int inj_mod = 0);
+  void putBallBounce();
+  void putBlockDiceChoice(SPlayer* p);
+  void putBlockFollowChoice(SPlayer* p);
+  void putBlockPushChoice(SPlayer* p);
+  void putBlockRoll(SPlayer* p);
+  void putCatchBallRoll(SPlayer* p, bool success);
+  void putDodgeRoll(SPlayer* p, bool success);
+  void putMove(SPlayer* p, Position aim);
+  void putPickUpRoll(SPlayer* p, bool success);
+  void putPushInTheCrowd(SPlayer* p);
+  void putStandUpRoll(SPlayer* p, bool success);
+  void putThrowRoll(SPlayer* p, int success);
+
+  //! @brief Processes elementary actions from the stack.
+  void process(bool reroll = false, int choice = -1);
+
+private:
+
+  enum eElementaryAction {
+    EA_BALL,          ///< Bounce the ball.
+    EA_BLOCKDICE,     ///< Choose a block dice.
+    EA_BLOCKFOLLOW,   ///< Choose to follow or not after a block-push.
+    EA_BLOCKPUSH,     ///< Choose where to push.
+    EA_MOVE,          ///< Move a player.
+    EA_PUSHOFFFIELD,  ///< Push a player in the crowd.
+    EA_ROLL,          ///< Apply dice roll, or roll dice.
+  };
+  struct ElementaryAction {
+    enum eElementaryAction type;
+    enum eRoll roll_type;
+    SPlayer* player;
+    int success;
+    int modifier;
+    int next_mod;
+    Position aim_position;
+    /* Still unused:
+    enum eSkill skill;
+    enum eStatus status;
+    SPlayer* target_player;
+    */
+  };
+  typedef std::vector<ElementaryAction> ElementaryActionList;
+
+  void put(ElementaryAction ea);
+
+  SRules* r_;
+  Dice* d_;
+  ElementaryActionList eal_;
+};
+
+#endif /* !SACTIONHANDLER_HH_ */
+
