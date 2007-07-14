d88c46f206839faff273407e3a573b10030e6b89
victor@14d9d42f-b097-442a-9da6-5b64999cc983 <victor@14d9d42f-b097-442a-9da6-5b64999cc983>
2007-07-14 09:07:52 +0000

    Update.

--- build-aux/build_prologin.sh
+++ build-aux/build_prologin.sh
@@ -13,8 +13,22 @@
 
 test -f configure.ac || exit 1
 
-sed -ir 's/^SUBDIRS.*+= doc.*//' Makefile.am
-rm README
-mv README.stechec README
+sed -ir '/^SUBDIRS.*+= doc/ { d }' Makefile.am
+sed -ir 's/README.stechec //' Makefile.am
+sed -ir '/BUILD_bb5/ { d }' Makefile.am
 
+if [ -f README.stechec ]; then
+    rm README
+    mv README.stechec README
+fi
+
+sed -ir '/doc\/Makefile/ { d }; /doc\/doc.doxy/ { d }' configure.ac
 sed -ir 's/modules="bb5"/modules="ant,prolo2005,prolo2006,prolo2007"/' configure.ac
+sed -ir 's!http://projects.nekeme.net/projects/tbt/newticket!info@prologin.org!' configure.ac
+sed -i '/^STECHEC_ADDMODULE(\[bb5\])/ { d }' configure.ac
+sed -ir '/^DEPENDS_bb5/ { d }' configure.ac
+sed -nir '/^if test x\$BUILD_bb5/ { :nn /^fi/ { d }; n; b nn }; p' configure.ac
+sed -ir '/Building with paragui/ { d }; /Building with readline/ { d }' configure.ac
+
+# ???
+rm -f configure.acr Makefile.amr
