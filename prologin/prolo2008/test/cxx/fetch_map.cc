1696025ec0ae640bc94e77129f95280ae6601ed4
kjus@14d9d42f-b097-442a-9da6-5b64999cc983 <kjus@14d9d42f-b097-442a-9da6-5b64999cc983>
2008-02-07 19:27:54 +0000

    un champion de test pour prolo2008 qui regarde juste le contenu de la map

---
+++ prologin/prolo2008/test/cxx/fetch_map.cc
@@ -0,0 +1,49 @@
+///
+// This file has been generated, if you wish to
+// modify it in a permanent way, please refer
+// to the script file : gen/generator_cxx.rb
+//
+
+#include "prologin.hh"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+///
+// Fonction appele au debut de la partie
+//
+void init_game()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appele a chaque tour
+//
+void play_turn()
+{
+  // verification des elements statiques du terrain
+  assert( type_case(0,0) == NORMAL );
+  assert( type_case(1,0) == MUR );
+  assert( type_case(0,1) == NORMAL );
+  assert( type_case(4,5) == MUR );
+  assert( type_case(2,0) == NORMAL );
+  assert( type_case(3,0) == NORMAL );
+  assert( type_case(4,0) == NORMAL );
+  assert( type_case(0,4) == TROU );
+  assert( type_case(2,2) == TROU );
+  
+  // verifications balles
+  assert( balle(1,3) );
+  assert( balle(2,0) );
+  assert( balle(2,5) );
+  assert( !balle(0,0));
+  assert( !balle(0,4));
+  assert( !balle(4,3));
+
+  //bornes :
+  assert( balle(-1,1) == BAD_ARGUMENT );
+  assert( balle(5,4) == BAD_ARGUMENT );
+  assert( type_case(5,4) == BAD_ARGUMENT );
+
+}
