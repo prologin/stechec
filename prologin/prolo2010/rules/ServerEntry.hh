b6be7f29bc0c07fb739c4d8c98467c8b7b88a66a
Pierre "delroth" Bourdon <delroth@gmail.com>
2010-02-21 23:22:18 +0100

    Added empty files which will be filled later with class declarations/definitions.

---
+++ prologin/prolo2010/rules/ServerEntry.hh
@@ -0,0 +1,16 @@
+/*
+** Stechec project is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** The complete GNU General Public Licence Notice can be found as the
+** `NOTICE' file in the root directory.
+**
+** Copyright (C) 2010 Prologin
+*/
+
+#ifndef SERVERENTRY_HH_
+# define SERVERENTRY_HH_
+
+#endif
