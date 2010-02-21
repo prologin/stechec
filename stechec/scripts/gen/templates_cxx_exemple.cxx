c1523b04f577bbe18c0891e3d35616f11b668563
Pierre "delroth" Bourdon <delroth@gmail.com>
2010-02-21 04:36:29 +0100

    Removed a now useless file.

--- stechec/scripts/gen/templates_cxx_exemple.cxx
+++
@@ -1,9 +0,0 @@
-
-
-template<typename Lang, typename Cxx>
-void lang2cxx_array(Lang* tab, size_t len, std::vector<Cxx>& vect)
-{
-  vect.reserve(len);
-  for (size_t i = 0; i < len; ++i)
-    vect.push_back( lang2cxx<Lang, Cxx>tab[i] );
-}
