35e94f7593a324bdaeb617983815492eb21a711a
Pierre "delroth" Bourdon <delroth@gmail.com>
2010-03-22 23:35:21 +0100

    Added the template of an interactive ASCII interface.

---
+++ prologin/prolo2010/asciiinter/champion.cc
@@ -0,0 +1,40 @@
+///
+// This file has been generated, if you wish to
+// modify it in a permanent way, please refer
+// to the script file : gen/generator_cxx.rb
+//
+
+#include "prologin.hh"
+
+///
+// Fonction appellée au début de la partie.
+//
+void init_game()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appellée pour la phase de retrait de KO.
+//
+unite retirer_ko()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appellée pour la phase de jeu.
+//
+void jouer()
+{
+  // fonction a completer
+}
+
+///
+// Fonction appellée à la fin de la partie.
+//
+void end_game()
+{
+  // fonction a completer
+}
+
