e14017c02721da5a15f7e67da84856c22a6f9dc1
max@max-laptop <max@max-laptop>
2010-04-17 20:43:40 +0200

    wip removing debug messages nicer printer management of one action : spawn

--- prologin/prolo2010/asciiinter/champion.cc
+++ prologin/prolo2010/asciiinter/champion.cc
@@ -1,8 +1,15 @@
-///
-// This file has been generated, if you wish to
-// modify it in a permanent way, please refer
-// to the script file : gen/generator_cxx.rb
-//
+/*
+** Stechec project is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** The complete GNU General Public Licence Notice can be found as the
+** `NOTICE' file in the root directory.
+**
+** Copyright (C) 2010 Prologin
+** @author Maxime Audouin <maxime.audouin@mlstate.com>
+*/
 
 #include <prologin.hh>
 #include "view.hh"
@@ -10,7 +17,7 @@
 #include <cstdio>
 #include <vector>
 #include <cstring>
-
+#include <assert.h>
 
 ///
 // Fonction appellée au début de la partie.
@@ -19,12 +26,22 @@ void init_game()
 {
 }
 
-char * string_of_type(type_unite t){
+static char * string_of_type(type_unite t){
   switch(t){
   case PERROQUET: return "perroquet";
   case SINGE: return "singe";
   case CHAT: return "chat";
   case KANGOUROU: return "kangourou";
+  }
+}
+
+static type_unite unit_of_int(int i){
+  switch(i){
+  case 1: return PERROQUET;
+  case 2: return SINGE;
+  case 3: return CHAT;
+  case 4: return KANGOUROU;
+  default: assert(false);
   }
 }
 
@@ -57,9 +74,24 @@ unite retirer_ko()
   return u[n];
 }
 
-///
-// Fonction appellée pour la phase de jeu.
-//
+unite choisir_unite(position p){
+  std::vector<unite> u = unites();
+  for (int  i = 0; i < u.size(); i++){
+    if (u[i].pos.x == p.x && u[i].pos.y == p.y)
+      return u[i];
+  }
+  assert(false);
+}
+
+bool contient_unite(position p){
+  std::vector<unite> u = unites();
+  for (int  i = 0; i < u.size(); i++){
+    if (u[i].pos.x == p.x && u[i].pos.y == p.y)
+      return true;
+  }
+  return false;
+}
+
 void jouer()
 {
   view();
@@ -67,8 +99,8 @@ void jouer()
   std::vector<unite> u = unites();
   for (int i = 0 ; i < u.size(); i++){
     if (!u[i].ennemi){
-      printf("indice=%d, type=%s, pa=%d, ko=%d, pos=(%d, %d)\n",
-	     i, string_of_type(u[i].type_unite_actuel), u[i].pa,
+      printf("type=%s, pa=%d, ko=%d, pos=(%d, %d)\n",
+	     string_of_type(u[i].type_unite_actuel), u[i].pa,
 	     u[i].ko, u[i].pos.x, u[i].pos.y);
     }
   }
@@ -79,7 +111,18 @@ void jouer()
     char buffer[80];
     fgets(buffer, 80, stdin);
     if (sscanf(buffer, "from (%d, %d) to (%d, %d)\n", &from.x, &from.y, &to.x, &to.y) == 4){
-      printf("OK");
+      if (contient_unite(from)){
+	afficher_erreur(deplacer(choisir_unite(from), to));
+      }else{
+	printf("aucune unite ici !");
+      }
+    }else if (strcmp(buffer, "spawn\n" == 0)){
+	do{
+	  printf("1 pour perroquet\n2 pour singe\n 3 pour chat\n4 pour kangourou\n");
+	  fgets(buffer, 80, stdin);
+	  if (sscanf(buffer, "%d", &n) != 1) n = 0; // bad value
+	} while (n < 1 || n > 4);
+        afficher_erreur(spawn(unit_of_int(n)));
     }else if (strcmp(buffer, "end\n") == 0){
       break;
     }
