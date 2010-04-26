f130650f70ad6865747b1f628aa3eab5944e5b93
Sylvain "Magicking" Laurent <syllaur@gmail.com>
2010-04-26 02:02:31 +0200

    Searching for an empty spawn.

--- prologin/prolo2010/tests/cs/prologin.cs
+++ prologin/prolo2010/tests/cs/prologin.cs
@@ -6,22 +6,34 @@ using System;
 
 namespace Prologin {
 
-	class Prologin {
+	class Prologin : Api {
 
 		public void InitGame()
 		{
 		}
 
+        bool cmpPos(Position a, Position b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
 		public Position RetirerKo()
 		{
-            Unites[] t = Unites();
-            for (int i = 0; t.Length; i++)
+            Unite[] t = Unites();
+            for (int i = 0; i < t.Length; i++)
                 if (!t[i].Ennemi)
-            return new Position();
+                    return t[i].Pos;
+            return new Position(); //Ne devrait jamais arriver
 		}
 
 		public void Jouer()
 		{
+            Unite[] t = Unites();
+            Position posSpawn = PosSpawn(false);
+            for (int i = 0; i < t.Length; i++)
+                if (!cmpPos(t[i].Pos, posSpawn)) //Faux si le Spawn est occupe
+                {
+                }
 		}
 
 		public void EndGame()
