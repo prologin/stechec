8b0960d2fafeef075fb8fc350841fb6b26498053
Sylvain "Magicking" Laurent <syllaur@gmail.com>
2010-04-28 01:20:07 +0200

    Working on...

--- prologin/prolo2010/tests/cs/prologin.cs
+++ prologin/prolo2010/tests/cs/prologin.cs
@@ -20,20 +20,42 @@ namespace Prologin {
 		public Position RetirerKo()
 		{
             Unite[] t = Unites();
-            for (int i = 0; i < t.Length; i++)
-                if (!t[i].Ennemi)
-                    return t[i].Pos;
+            foreach (Unite item in t)
+                if (!item.Ennemi)
+                    return item.Pos;
             return new Position(); //Ne devrait jamais arriver
 		}
+
+        Unite getPosUnite(Position a)
+        {
+            foreach (Unite item in t)
+                if (cmpPos(t.Pos, a))
+        }
+
+        Position minDistPost(Position a, Position b, Position, Position goalPos)
+        {
+            Position tmp;
+            int distA = Math.Abs(goalPos.X - a.X) + Math.Abs(goalPos.Y - a.Y);
+            int distB = Math.Abs(goalPos.X - b.X) + Math.Abs(goalPos.Y - b.Y);
+
+            if (distA > distB)
+                return b;
+
+            return a;
+        }
 
 		public void Jouer()
 		{
             Unite[] t = Unites();
             Position posSpawn = PosSpawn(false);
-            for (int i = 0; i < t.Length; i++)
+            
+
+            foreach (Unite item in t)
                 if (!cmpPos(t[i].Pos, posSpawn)) //Faux si le Spawn est occupe
-                {
-                }
+                    Spawn(TypeUnite.CHAT);      //Accessible au prochain tour
+            foreach (Unite item in t)
+                if (!item.Ennemi)
+                
 		}
 
 		public void EndGame()
