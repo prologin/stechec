8f875c493033c18fba2b162b3ac93d50aad76f34
Sylvain "Magicking" Laurent <syllaur@gmail.com>
2010-04-29 02:39:27 +0200

    The champion should now spawn a cat and rush to Titi but not yet tested.

--- prologin/prolo2010/tests/cs/prologin.cs
+++ prologin/prolo2010/tests/cs/prologin.cs
@@ -36,12 +36,14 @@ namespace Prologin {
             throw new Exception(); //No unit
         }
 
+        int distPos(Position a, Position b)
+        {
+            return Math.Max(Math.Abs(a.X - b.X), Math.Abs(a.Y - b.Y));
+        }
+
         Position minDistPos(Position a, Position b, Position goalPos)
         {
-            int distA = Math.Abs(goalPos.X - a.X) + Math.Abs(goalPos.Y - a.Y);
-            int distB = Math.Abs(goalPos.X - b.X) + Math.Abs(goalPos.Y - b.Y);
-
-            if (distA > distB)
+            if (distPos(goalPos, a) > distPos(goalPos, b))
                 return b;
 
             return a;
@@ -56,27 +58,59 @@ namespace Prologin {
             }
             catch
             {
-                Spawn(TypeUnite.CHAT);      //Accessible au prochain tour
+                Spawn(TypeUnite.CHAT);      //Will be available next turn
             }
 
-            Unite uTiti = new Unite(); //prevent warning
+            Unite uTiti = new Unite();    //Prevent warning
             foreach (Unite item in t)
                 if (item.Ennemi && item.VraiTypeUnite == TypeUnite.PERROQUET)
                 {
                     uTiti = item;
                     break;
                 }
-
-            if (t.Length > 0)
+            bool MADE_ACTIONS = true;
+            while (NombrePc() > 0 && MADE_ACTIONS)
             {
-                Unite min;
-                Position minPos;
-                min = t[0];
-                foreach (Unite item in t)
-                    if (item.VraiTypeUnite == item.TypeUniteActuel && item.TypeUniteActuel == TypeUnite.CHAT && !item.Ennemi)
+                    MADE_ACTIONS = false;
+                    if (t.Length > 0)
                     {
-                        minPos = minDistPos(min.Pos, item.Pos, uTiti.Pos);
-                        min = getPosUnite(minPos); //Should not thrown error
+                        Unite min = null;
+                        foreach (Unite item in t)
+                            if (!item.Ennemi && item.VraiTypeUnite == item.TypeUniteActuel &&
+                                 item.TypeUniteActuel == TypeUnite.CHAT && item.Ko < 0 && item.Pa > 0)
+                                if (min != null)
+                                    min = getPosUnite(minDistPos(min.Pos, item.Pos, uTiti.Pos));  //Seeking the closest cat to ennemy's titi
+                                else
+                                    min = item;
+                        if (min != null)
+                            if (distPos(min.Pos, uTiti.Pos) == 1 && Attaquer(min.Pos, uTiti.Pos) == Erreur.OK)
+                                break;  //We wone
+                            else    //We have to move our cat to the closest position to titi
+                            {
+                                Position closest = min.Pos;
+                                TailleTerrain mapbound = TailleTerrainActuelle();
+                                int minx = (min.Pos.X - min.Pa) >= mapbound.MinCoord ? min.Pos.X - min.Pa : mapbound.MinCoord;
+                                int minyI = (min.Pos.Y - min.Pa) >= mapbound.MinCoord ? min.Pos.Y - min.Pa : mapbound.MinCoord;
+
+                                int maxx = (min.Pos.X + min.Pa) < mapbound.MaxCoord ? min.Pos.X + min.Pa : mapbound.MaxCoord;
+                                int maxy = (min.Pos.Y + min.Pa) < mapbound.MaxCoord ? min.Pos.Y + min.Pa : mapbound.MaxCoord;
+                                for (; minx < maxx; minx++)
+                                    for (int miny = minyI; miny < maxy; miny++)
+                                    {
+                                        Position tmppos = new Position();
+                                        tmppos.X = minx;
+                                        tmppos.Y = miny;
+                                        try
+                                        {
+                                            getPosUnite(tmppos); //Will throw an error in case of no Unite
+                                        }
+                                        catch
+                                        {
+                                            closest = minDistPos(closest, tmppos, uTiti.Pos);
+                                        }
+                                    }
+                                AfficherErreur(Deplacer(min.Pos, closest));
+                            }
                     }
             }
 		}
