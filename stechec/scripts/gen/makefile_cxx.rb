2f6f2c3cf0bb216426434afc0ee34bcd04670667
Pierre "delroth" Bourdon <delroth@gmail.com>
2010-04-29 04:30:10 +0200

    Adapted the C++ meta Makefile to the new architecture.

--- stechec/scripts/gen/makefile_cxx.rb
+++ stechec/scripts/gen/makefile_cxx.rb
@@ -11,65 +11,22 @@
 #
 
 class CxxMakefile
-  def initialize
-    @makefile = <<EOF
-#####################
-# Macro Definitions #
-#####################
-CXX 	= g++
-MAKE 	= make
-LIMEOBJ ?= ../includes/main.o
-OBJS 	= ${SRC:.cc=.o} $(LIMEOBJ)
-RM 	= /bin/rm -f
-
-CFLAGS = -fPIC
-CXXFLAGS  = -fPIC -Wall ${MY_CXXFLAGS}
-
-##############################
-# Basic Compile Instructions #
-##############################
-
-all: ${NAME}
-
-${NAME}: ${OBJS} ${MY_LIBS}
-\t$(CXX) #{TARGET_GCC_LINK} ${OBJS} ${MY_LIBS} -o ${NAME} $(CHECK_CHEAT)
-\t@echo Finished
-
-clean:
-\t@echo "Cleaning..."
-\t${RM} ${OBJS} *~ \#*\#
-
-distclean: clean
-\t${RM} ${NAME} #{$conf['conf']['player_filename']}.tgz
-
-tar:
-\ttar cvzf #{$conf['conf']['player_filename']}.tgz $(SRC) *.hh
-EOF
-  end
-
-  def build_client(path)
-    f = File.new(path + "makecxx", 'w')
-    f.puts "# -*- Makefile -*-"
-    f.print @makefile
-    f.close
-  end
-
   def build_metaserver(path)
     f = File.new(path + "Makefile-cxx", 'w')
+    target = $conf['conf']['player_lib']
     f.print <<-EOF
 # -*- Makefile -*-
 
-INCL      = $(wildcard *.hh)
-SRC       = $(wildcard *.cc *.cpp)
-NAME      = #{$conf['conf']['player_lib']}.so
+lib_TARGETS = #{target}
 
-MY_CXXFLAGS = -O2
+# Tu peux rajouter des fichiers sources, headers, ou changer
+# des flags de compilation.
+#{target}-srcs = $(wildcard *.cc)
+#{target}-cxxflags = -O2 -Wall
 
-CHECK_CHEAT = `while read i; do echo -Wl,--wrap=$$i; done < forbidden_fun-c`
-LIMEOBJ   = stechec_lime.o
-
+V=1
+include $(MFPATH)/rules.mk
     EOF
-    f.print @makefile
     f.close
   end
 
