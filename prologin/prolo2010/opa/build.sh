63bf346c807d9d942d39180ca0eacd27a3401c54
max@max-laptop.localnet <max@max-laptop.localnet>
2010-05-29 14:00:44 +0200

    fixing build script for package users

--- prologin/prolo2010/opa/build.sh
+++ prologin/prolo2010/opa/build.sh
@@ -1,4 +1,4 @@
-time ~/opa/opa/opa.exe --verbose \
+time opa --verbose \
   lib.opa lib_2players.opa wait_game.opa lib_games.opa server.opa \
   interface.opa types_com.opa page_help.opa prologin.opa\
 && \
