9db1b5490a517db70ce857a4ffe4f156127e3d53
max@max-laptop.localnet <max@max-laptop.localnet>
2010-06-11 00:52:20 +0200

    adding "filet" to ocaml champion

--- prologin/summer2010/tests/caml/prologin.ml
+++ prologin/summer2010/tests/caml/prologin.ml
@@ -15,6 +15,7 @@ module List = struct
 	if n = i then [f n]
 	else (f n) :: (aux (n + 1))
       in aux 0;;
+
   let carthesien l1 l2 =
     List.fold_left
       (fun acc i1 ->
@@ -22,7 +23,12 @@ module List = struct
 	   (fun acc i2 ->
 	      (i1, i2) :: acc
 	   ) acc l2
-      ) [] l1
+      ) [] l1;;
+
+  let find_opt f li =
+    try
+      Some (List.find f li)
+    with Not_found -> None
 end
 
 module Tuple = struct
@@ -85,6 +91,11 @@ let position_occupee p =
 (*  let () = show_unites u in *)
     List.exists (fun u -> u.pos_unite = p) u
 
+let player_at p =
+  let u = unites () |> Array.to_list in
+(*  let () = show_unites u in *)
+    List.find_opt (fun u -> u.pos_unite = p) u
+
 let direction u p =
   let (ux, uy) = u.pos_unite in
   let (px, py) = p.pos_piece in
@@ -121,45 +132,125 @@ let direction u p =
 	in Some (x, y)
     | None -> None
 
+let manger_simple u money =
+  begin
+    clean ();
+    let opt = List.fold_left
+      (fun opt piece ->
+	 if cant_go u piece.pos_piece then opt
+	 else match opt with
+	   | None -> Some piece
+	   | Some autre_piece ->
+	       Some (better_piece u piece autre_piece)
+      ) None money
+    in match opt with
+      | Some p ->
+	  begin
+	    Printf.printf "manger une piece\n";
+	    clean();
+	    deplacer u p.pos_piece;
+	  end
+      | None ->
+	  let () = Printf.printf "aller vers la piece la plus proche\n" in
+	  let () = clean() in
+	  let opt =
+	    List.fold_left
+	      (fun opt piece ->
+		 match opt with
+		   | None -> Some piece
+		   | Some autre_piece ->
+		       Some (plus_proche u piece autre_piece)
+	      ) None money
+	  in match opt with
+	    | None -> ()
+	    | Some p ->
+		match direction u p with
+		  | Some p -> deplacer u p
+		  | None -> ()
+  end
+let prix objet = (proprietes_objet objet).cout;;
+
+let utiliser_objet u money =
+  let unites = unites () |> Array.to_list in
+  let upos = u.pos_unite in
+  let (ux, uy) = upos in
+  let lec f = List.filter (fun p -> (f p.pos_piece) = (f upos)) money in
+  let ligne (a, b) = b in
+  let colone (a, b) = a in
+  let gauche, droite = List.partition (fun p -> (colone p.pos_piece) < (colone upos) ) (lec ligne) in
+  let haut, bas = List.partition (fun p -> (ligne p.pos_piece) < (ligne upos) ) (lec colone) in
+  let hd = ((fst upos), 0) in
+  let bd = ((fst upos), taille_terrain - 1) in
+  let gd = (0, (snd upos)) in
+  let dd = (taille_terrain - 1, (snd upos)) in
+  let li = [
+    (hd, haut, (fun (x, y) -> x = ux && y < uy));
+    (bd, bas, (fun (x, y) -> x = ux && y > uy));
+    (gd, gauche, (fun (x, y) -> x < ux && y = uy));
+    (dd, droite, (fun (x, y) -> x > ux && y = uy))
+  ] in
+    List.fold_left
+      (fun acc (direction, pieces, filtre) ->
+	 acc || (
+	   let unite_touchee =
+	     List.fold_left
+	       (fun opt u ->
+		  if filtre u.pos_unite then
+		    match opt with
+		      | None -> Some u
+		      | Some u2 ->
+			  Some (
+			    if distance upos u.pos_unite > distance upos u2.pos_unite
+			    then u2
+			    else u)
+		  else opt
+	       )
+	       None
+	       unites
+	   in
+	   let moi_touche, lui_touche = match unite_touchee with
+	     | None -> false, false
+	     | Some u -> let t = mon_equipe () in (t = u.team, t <> u.team)
+	   in
+	   let pieces =
+	     match unite_touchee with
+	       | Some u2 ->
+		   let dref = distance upos u2.pos_unite in
+		     List.filter
+		       (fun p -> distance upos p.pos_piece < dref )
+		       pieces
+	       | None -> pieces
+	   in
+	   let () =
+	     begin
+	       if moi_touche then
+		 Printf.printf "MOI TOUCHE !\n"
+	     end in
+	     if moi_touche then false else
+	       let gain =
+		 let pieces = List.map (fun p -> p.valeur) pieces in
+		   List.fold_left (+) 0 pieces in
+	       let () = Printf.printf "gain = %d prix = %d\n" gain (prix Filet)
+	       in
+		 if gain > (prix Filet) then
+		   let () = begin
+		     Printf.printf "utiliser objet ! (de %d, %d en %d, %d)\n" ux uy (fst direction) (snd direction);
+		     List.iter (fun p -> Printf.printf "on capture la piece %d, %d\n" (fst p.pos_piece) (snd p.pos_piece) ) pieces;
+		     clean();
+		     acheter_objet upos Filet |> afficher_erreur;
+		     utiliser upos direction |> afficher_erreur;
+		     clean();
+		   end
+	       in true
+		 else false
+	 )
+      ) false li
+
 let move_player (u:unite) =
   Printf.printf "recuperation des pieces...\n";
   clean();
   let money = pieces_en_jeu () |> Array.to_list in
-    begin
-      clean ();
-      let opt = List.fold_left
-	(fun opt piece ->
-	   if cant_go u piece.pos_piece then opt
-	   else match opt with
-	     | None -> Some piece
-	     | Some autre_piece ->
-		 Some (better_piece u piece autre_piece)
-	) None money
-      in match opt with
-	| Some p ->
-	    begin
-	      Printf.printf "manger une piece\n";
-	      clean();
-	      deplacer u p.pos_piece;
-	    end
-	| None ->
-	    let () = Printf.printf "aller vers la piece la plus proche\n" in
-	    let () = clean() in
-	    let opt =
-	      List.fold_left
-		(fun opt piece ->
-		   match opt with
-		     | None -> Some piece
-		     | Some autre_piece ->
-			 Some (plus_proche u piece autre_piece)
-		) None money
-	    in match opt with
-	      | None -> ()
-	      | Some p ->
-		  match direction u p with
-		    | Some p -> deplacer u p
-		    | None -> ()
-    end
+    if not (utiliser_objet u money) then manger_simple u money
 ;;
 
 let deplacer () =
