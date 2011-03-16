0656861d08b40b7b59e0d40feefdbcfe29e6d0bb
Pierre-Marie de Rodat <pmderodat@lse.epita.fr>
2011-03-17 00:33:58 +0100

    Continued the C++ IA to complete the test suite

--- prologin/prolo2011/tests/cxx/prologin.cc
+++ prologin/prolo2011/tests/cxx/prologin.cc
@@ -16,6 +16,45 @@ void init_game()
   std::cout << "equipe : " << mon_equipe() << std::endl;
 }
 
+void my_print_trainees()
+{
+    // Affichage des trainees
+    std::vector<trainee_moto> t = trainees_moto();
+    for (unsigned int i = 0; i < t.size(); i++)
+    {
+        trainee_moto tr = t.at(i);
+        std::cout << "trainee : " << i << std::endl;
+        for (unsigned int j = 0; j < tr.emplacement.size(); j++)
+        {
+            position p = tr.emplacement.at(j);
+            std::cout << "  " << p.x << " " << p.y << std::endl;
+        }
+    }
+}
+
+void my_print_sources()
+{
+    std::cout << "Sources:" << std::endl;
+    std::vector<source_energie> sources = sources_energie();
+    for (unsigned int i = 0; i < sources.size(); ++i)
+    {
+        source_energie& s = sources[i];
+        std::cout << "  source[" << s.id << "] @ ("
+                  << s.pos.x << ", " << s.pos.y << ") : "
+                  << s.coef << std::endl;
+    }
+}
+
+void my_print_scores()
+{
+    // Affichage des scores
+    std::vector<int>    s = scores();
+    std::cout << "scores:";
+    for (unsigned int i = 0; i < s.size(); ++i)
+        std::cout << " " << i << "->" << s[i];
+    std::cout << std::endl;
+}
+
 ///
 // Fonction appellée pour la phase de jeu
 //
@@ -24,17 +63,12 @@ void jouer()
   // position init = {99, 99};
   // position end = {99, 98};
   // deplacer(1, init, end);
-  
-std::cout << "tour : " << tour_actuel() << std::endl;
-  std::vector<trainee_moto> t = trainees_moto();
-  for (int i = 0; i < t.size(); i++){
-    trainee_moto tr = t.at(i);
-    std::cout << "trainee : " << i << std::endl;
-    for (int j = 0; j < tr.emplacement.size(); j++){
-      position p = tr.emplacement.at(j);
-      std::cout << "  " << p.x << " " << p.y << std::endl;
-    }
-  }
+
+    std::cout << "tour : " << tour_actuel() << std::endl;
+
+    my_print_trainees();
+    my_print_sources();
+    my_print_scores();
 }
 
 ///
