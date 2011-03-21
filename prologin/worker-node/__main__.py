6d2c9db4c22e7038fa9dc81777410a5554041eae
Pierre Bourdon <delroth@gmail.com>
2011-03-21 16:38:32 +0100

    Send more useful informations with the worker heartbeat
    The master needs to know at least where to connect (host/port), the number of
    available slots on the worker, and the number of maximum slots to compute the
    current load and to display fancy stats. Send these informations via the
    regular heartbeat.

--- prologin/worker-node/__main__.py
+++ prologin/worker-node/__main__.py
@@ -24,15 +24,21 @@ import gevent.socket
 gevent.monkey.patch_all()
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import socket
 import xmlrpclib
 
 class WorkerNode(object):
     def __init__(self, config):
         self.master = self.create_master_connection(config)
         self.interval = config['master']['interval']
-        self.infos = ('localhost', 8068)
+        self.hostname = socket.gethostname()
+        self.port = config['worker']['port']
+        self.slots = self.max_slots = config['worker']['available_slots']
 
         self.heartbeat_greenlet = gevent.spawn(self.send_heartbeat)
+
+    def get_worker_infos(self):
+        return (self.hostname, self.port, self.slots, self.max_slots)
 
     def create_master_connection(self, config):
         host, port = (config['master']['host'], config['master']['port'])
@@ -42,7 +48,7 @@ class WorkerNode(object):
     def send_heartbeat(self):
         while True:
             try:
-                self.master.heartbeat('secret', self.infos)
+                self.master.heartbeat('secret', self.get_worker_infos())
             except gevent.socket.error:
                 print 'master down, retrying heartbeat in %ds' % self.interval
             gevent.sleep(self.interval)
@@ -55,13 +61,27 @@ class WorkerNodeProxy(object):
     def __init__(self, node):
         self.node = node
 
+# TODO: Replace that with a YAML parsing function
+def read_config(filename):
+    return {
+        'master': {
+            'host': 'localhost',
+            'port': 8067,
+            'interval': 2,
+            'secret': 'foobar',
+        },
+        'worker': {
+            'port': 8068,
+            'available_slots': 20,
+        }
+    }
+
 if __name__ == '__main__':
+    config = read_config("")
     s = SimpleXMLRPCServer(('localhost', 8068), logRequests=False)
     s.register_introspection_functions()
 
-    worker = WorkerNode({ 'master': {'host': 'localhost',
-                                     'port': 8067,
-                                     'interval': 2}})
+    worker = WorkerNode(config)
     s.register_instance(WorkerNodeProxy(worker))
 
     s.serve_forever()
