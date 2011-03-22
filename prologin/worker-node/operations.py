d0e05073abb5ab2868f74c18a9c079a4add1e85a
Pierre Bourdon <delroth@gmail.com>
2011-03-22 07:28:02 +0100

    Add a new operations module to the worker node
    The worker node can do several things related to Stechec, including champion
    compilation and match launching. To separate the code related to these actions
    from the main server code, put them in another module.

---
+++ prologin/worker-node/operations.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+# This file is part of Stechec.
+#
+# Copyright (c) 2011 Pierre Bourdon <pierre.bourdon@prologin.org>
+# Copyright (c) 2011 Association Prologin <info@prologin.org>
+#
+# Stechec is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Stechec is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Stechec.  If not, see <http://www.gnu.org/licenses/>.
+
+def compile_champion(config, dir_path, user, champ_id):
+    """
+    Compiles the champion at $dir_path/champion.tgz to $dir_path/champion.so.
+
+    Returns a tuple (ok, output), with ok = True/False and output being the
+    output of the compilation script.
+    """
+    # TODO
+    return (True, "")
