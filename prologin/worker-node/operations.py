86b331d07c09952e1120fe728cb6892c82cea206
Pierre Bourdon <delroth@lse.epita.fr>
2011-04-19 11:41:49 +0200

    Implemented the dumping in the worker node

--- prologin/worker-node/operations.py
+++ prologin/worker-node/operations.py
@@ -23,10 +23,12 @@ import errno
 import fcntl
 import gevent
 import gevent.socket
+import gzip
 import paths
 import os
 import os.path
 import sys
+import tempfile
 
 def communicate(cmdline, data=''):
     """
@@ -110,6 +112,43 @@ def spawn_server(cmd, path, match_id, callback):
     open(log_path, "w").write(stdout)
     callback(retcode, stdout, match_id)
 
+def spawn_dumper(cmd, path):
+    dump_path = tempfile.mktemp()
+    def set_path():
+        os.environ['DUMP_PATH'] = dump_path
+
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, preexec_fn=set_path)
+    fcntl.fcntl(p.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(p.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
+    p.stdin.close()
+
+    # Read stdout data
+    while True:
+        try:
+            chunk = p.stdout.read(4096)
+            if not chunk:
+                break
+        except IOError, ex:
+            if ex[0] != errno.EAGAIN:
+                raise
+            sys.exc_clear()
+        gevent.socket.wait_read(p.stdout.fileno())
+    p.stdout.close()
+
+    # Wait for process completion
+    while p.poll() is None:
+        gevent.sleep(0.05)
+
+    # Compress the dump and place it to its final destination
+    final_dump = os.path.join(path, "dump.json.gz")
+    final_fp = gzip.open(final_dump, "w")
+    fp = open(dump_path, "r")
+    for line in fp:
+        final_fp.write(line)
+    fp.close()
+    final_fp.close()
+    os.unlink(dump_path)
+
 def run_server(config, server_done, port, contest, match_id, opts):
     """
     Runs the Stechec server and wait for client connections.
@@ -120,10 +159,13 @@ def run_server(config, server_done, port, contest, match_id, opts):
     except OSError:
         pass
 
+    dumper = config['contest']['dumper']
+    nb_spectator = 0 if dumper == 'no' else 1
     config_path = os.path.join(path, "config_server.ini")
     config = '''
 [server]
 listen_port=%(port)d
+nb_spectator=%(nb_spectator)d
 verbose=2
 
 [%(contest)s]
@@ -133,6 +175,13 @@ verbose=2
     cmd = [paths.stechec_server, "-c", config_path]
     gevent.spawn(spawn_server, cmd, path, match_id, server_done)
     gevent.sleep(0.25) # let it start
+    if nb_spectator:
+        gevent.spawn(
+            spawn_dumper,
+            [paths.stechec_client, "-i", "0", "-r", "tron", "-a", paths.libdir,
+             "-l", dumper, "-d", "127.0.0.1", "-p", str(port), "-s"],
+            path
+        )
 
 def spawn_client(cmd, path, match_id, champ_id, tid, callback):
     retcode, stdout = communicate(cmd)
