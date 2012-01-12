89851257a57678c9f959b7c202d0e75d38509a64
Pierre Bourdon <delroth@gmail.com>
2012-01-12 10:50:50 +0100

    generators: add the Java/C++ interface skeleton generation

--- stechec/scripts/gen/generator_java.rb
+++ stechec/scripts/gen/generator_java.rb
@@ -1,3 +1,105 @@
+# -*- ruby -*-
+# Stechec project is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# The complete GNU General Public Licence Notice can be found as the
+# `NOTICE' file in the root directory.
+#
+# Copyright (C) 2011 Prologin
+
+class JavaCxxFileGenerator < CxxProto
+  def initialize
+    super
+    @lang = "C++ (for Java interface)"
+  end
+
+  def generate_header
+    @f = File.open(@path + @header_file, 'w')
+    print_banner 'generator_java.rb'
+
+    @f.puts <<-EOF
+#ifndef INTERFACE_HH_
+# define INTERFACE_HH_
+
+# include <jni.h>
+# include <vector>
+# include <string>
+
+    EOF
+
+    build_enums
+    build_structs
+
+    @f.puts "", 'extern "C" {', ""
+    for_each_fun do |fn|
+      @f.print cxx_proto(fn, "api_")
+      @f.puts ";"
+    end
+    @f.puts "}", "", "#endif // !INTERFACE_HH_"
+    @f.close
+  end
+
+  def build_common_wrappers
+    @f.puts "// TODO: common wrappers", ""
+  end
+
+  def build_enum_wrappers(enum)
+    name = enum['enum_name']
+    @f.puts "// TODO: wrapper for enum #{name}"
+  end
+
+  def build_struct_wrappers(str)
+    name = str['str_name']
+    @f.puts "// TODO: wrapper for struct #{name}"
+  end
+
+  def build_function(fn)
+    @f.puts "// TODO: wrapper for API function #{fn.name}"
+  end
+
+  def build_user_function(fn)
+    @f.puts "// TODO: wrapper for user function #{fn.name}"
+  end
+
+  def generate_source
+    @f = File.open(@path + @source_file, 'w')
+    print_banner "generator_java.rb"
+
+    @f.puts <<-EOF
+#include "interface.hh"
+
+static void _init_vm();
+
+    EOF
+
+    build_common_wrappers
+    for_each_enum { |e| build_enum_wrappers e }
+    for_each_struct { |s| build_struct_wrappers s }
+    for_each_fun { |fn| build_function fn }
+
+    @f.puts <<-EOF
+
+// TODO: Java/C++ interface functions
+
+    EOF
+
+    for_each_user_fun { |fn| build_user_function fn }
+
+    @f.close
+  end
+
+  def build
+    @path = Pathname.new($install_path) + "java"
+    @header_file = "interface.hh"
+    @source_file = "interface.cc"
+
+    generate_header
+    generate_source
+  end
+end
+
 class JavaFileGenerator < JavaProto
   def initialize
     super
@@ -27,7 +129,7 @@ class JavaFileGenerator < JavaProto
     @java_interface = 'Interface'
     @java_file = $conf['conf']['player_filename'].capitalize
 
-    # TODO: JavaCxxFileGenerator.new.build
+    JavaCxxFileGenerator.new.build
 
     ######################################
     ##  Interface.java file generating  ##
